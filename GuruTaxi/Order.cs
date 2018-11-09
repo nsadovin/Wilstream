f5ddc2a56a711ac43ef57ed1051339debc9737b4
Садовин Николай <vesdehod@mail.ru>
2018-11-09 23:44:44 +0300

    Добавлен проект GuruTaxi

---
+++ GuruTaxi/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Guru
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public Car car { get; set; }
+        public Driver driver { get; set; }
+        public Company company { get; set; }
+    }
+
+    public class Car
+    {
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+    }
+     
+    public class Driver
+    {
+
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public License license { get; set; }
+        public int experience { get; set; }
+    }  
+
+    public class License
+    {
+        public string country { get; set; }
+        public string number { get; set; }
+    }
+
+
+    public class Company
+    {
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string interviewSchedule { get; set; }
+        public string token { get; set; }
+    } 
+}
---
+++ GuruTaxi/api.ashx.cs
@@ -0,0 +1,91 @@
+﻿
+using log4net;
+using log4net.Config;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Guru
+{
+    /// <summary>
+    /// Сводное описание для api
+    /// </summary>
+    public class api : IHttpHandler
+    {
+        private static ILog log = LogManager.GetLogger("LOGGER");
+
+
+        public void ProcessRequest(HttpContext context)
+        {
+            XmlConfigurator.Configure();
+
+            StreamReader reader = new StreamReader(HttpContext.Current.Request.InputStream);
+            string requestFromPost = reader.ReadToEnd();
+            try
+            {
+                log.Debug("requestFromPost is " + requestFromPost);
+                var order = JsonConvert.DeserializeObject<Order>(requestFromPost);
+                bool result = saveOrder(order);
+            }
+            catch (SqlException ex)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("Error:" + ex.Message);
+                log.Error(ex);
+                context.Response.End();
+            }
+            catch (Exception ex)
+            {
+
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("Error:" + ex.Message);
+                log.Error(ex);
+                context.Response.End();
+            }
+
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("OK");
+        }
+
+        private bool saveOrder(Order order)
+        {
+             
+                System.Data.SqlClient.SqlConnection conn = null;
+
+                System.Configuration.ConnectionStringSettings settings =
+                System.Configuration.ConfigurationManager.ConnectionStrings["oktellConnectionString"];
+
+                SqlConnection myOdbcConnection = new SqlConnection(settings.ConnectionString);
+
+                var SqlStr = "INSERT INTO[oktell].[dbo].[WS_GuruTaxi] ([Gury_car_brand]   ,[Gury_car_model]  ,[Gury_driver_firstName]  ,[Gury_driver_lastName]  ,[Gury_driver_phone]  ,[Gury_driver_license_number]  ,[Gury_driver_license_country]   ,[Gury_driver_experience]   ,[Gury_Id]  ,[Gury_car_id]   ,[Gury_driver_id]    ,[Gury_company_id]  ,[Gury_company_brand]  ,[Gury_company_interviewSchedule]  ,[Gury_company_token])   VALUES ('"+ order.car.brand+ "','" + order.car.model + "'  ,'" + order.driver.firstName + "','" + order.driver.lastName + "','" + order.driver.phone + "','" + order.driver.license.number + "','" + order.driver.license.country + "','" + order.driver.experience + "' ," + order.id + "," + order.car.id + "," + order.driver.id + " ," + order.company.id + " ,'" + order.company.brand+ "' ,'" + order.company.interviewSchedule + "'   ,'" + order.company.token + "')";
+ 
+
+
+
+                SqlCommand myOdbcCommand = new SqlCommand(SqlStr, myOdbcConnection);
+                myOdbcCommand.CommandType = CommandType.Text;
+                myOdbcCommand.Connection.Open();
+                SqlDataReader myOdbcReader = myOdbcCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                myOdbcReader.Close();
+                myOdbcConnection.Close();
+
+                return true;
+ 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
