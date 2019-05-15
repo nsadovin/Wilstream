d2b8b7b4f65b75fe383e0c99d9c91394a0aec270
Садовин Николай <n_sadovin@step.ru>
2019-05-15 13:12:24 +0300

    Upadte

--- WebAPI/Models/OktellOperators.cs
+++ WebAPI/Models/OktellOperators.cs
@@ -10,6 +10,8 @@ using System.Web.Http;
 using System.Text;
 using log4net;
 using log4net.Config;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WebAPI.Models
 {
@@ -20,23 +22,62 @@ namespace WebAPI.Models
 
         public static IEnumerable<Operator> GetReadyOperators()
         {
-            
-            
+
+           
             var rslt = new List<Operator>();
             List<Guid> users_id = new List<Guid>();
-            users_id.Add(Guid.Parse("F947931E-CDDF-4BA4-BD0B-6E9171BA5824")); 
+          //  users_id.Add(Guid.Parse("F947931E-CDDF-4BA4-BD0B-6E9171BA5824")); 
+            users_id.AddRange(OktellOperators.GetOperatorsId().Select(r=>r.Item1).ToList());
+            log.Debug("Count user in task is " + users_id.Count);
 
             foreach (var user_id in users_id)
             {
                 var xml = request(user_id);
                 OktellOperators.log.Debug(xml);
                 var obj = Parse(xml);
-                rslt.Add(new Operator() { OperatorId = user_id, OperatorStatus = obj.Data.PropertySet.PropertySimple.FirstOrDefault(r => r.key == "state").name });
+                rslt.Add(new Operator() { OperatorId = user_id,
+                    OperatorStatus = obj.Data.PropertySet.PropertySimple.FirstOrDefault(r => r.key == "state").name== "usReady"
+                });
                 //rslt.Add(new Operator() { OperatorId = Guid.Empty, OperatorStatus = xml });
 
             }
 
             return rslt;
+        }
+
+
+        private static List<Tuple<Guid, String>> GetOperatorsId()
+        {
+            var rslt = new List<Tuple<Guid, String>>();
+            try
+            {
+                System.Data.SqlClient.SqlConnection conn = null;
+
+                System.Configuration.ConnectionStringSettings settings =
+                System.Configuration.ConfigurationManager.ConnectionStrings["oktell_ccwsConnectionString"];
+
+                SqlConnection myOdbcConnection = new SqlConnection(settings.ConnectionString);
+
+                SqlCommand myOdbcCommand = new SqlCommand();
+                myOdbcCommand.Connection = myOdbcConnection;
+                myOdbcCommand.CommandType = CommandType.Text;
+                myOdbcCommand.CommandText = "SELECT  t2.Id, t2.Name FROM [oktell_settings].[dbo].[A_TaskManager_Operators] t1 WITH(NOLOCK), oktell_cc_temp.dbo.A_Cube_CC_Cat_OperatorInfo t2 with(nolock) 	WHERE t1.OperatorId = t2.Id and TaskId in ('DCFB629F-6B42-4985-AC55-115EA9819F16')";
+                myOdbcCommand.Connection.Open();
+                SqlDataReader myOdbcReader = myOdbcCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                while (myOdbcReader.Read())
+                {
+                    rslt.Add(new Tuple<Guid, String>(myOdbcReader.GetGuid(0), myOdbcReader.GetString(1)));
+                }
+
+                myOdbcReader.Close();
+                myOdbcConnection.Close();
+                return rslt;
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                return new List<Tuple<Guid, String>>();
+            }
         }
 
         private static oktellxmlmapper Parse(string xml)
@@ -129,7 +170,7 @@ namespace WebAPI.Models
  <property_cdata key="returnvalue"><![CDATA[f947931e-cddf-4ba4-bd0b-6e9171ba5824]]></property_cdata>    </property_set>  </data></oktellxmlmapper>
              */
             string rslt = ""; 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://192.168.3.3:4055/execsvcscript?name=chat2desk_set_operator_to_task&async=0&startparam1=" + user_id);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://192.168.3.3:4055/execsvcscript?name=HotLinePhillips_chat2desk_set_operator_to_task&async=0&startparam1=" + user_id);
             string username = "sadovin";
             string password = "azsxdcfv890";
             string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username + ":" + password));
@@ -205,6 +246,6 @@ namespace WebAPI.Models
 
     public class Operator {
         public Guid OperatorId {get; set;}
-        public String OperatorStatus {get; set;}
+        public bool OperatorStatus {get; set;}
     }
 }
